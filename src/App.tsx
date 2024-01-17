8214d2f199b856eb4006c9930ad2b599209f69fd
Alex Johnson <johnsonav1992@gmail.com>
2024-01-17 06:23:07 -0600

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App () {
 
     return (
         <>
-            <div {...styleX.props(styles.test)}>
+            <div {...styleX.props(styles.test)} >
                 Hello
             </div>
         </>
