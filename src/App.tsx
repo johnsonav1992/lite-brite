ae9ad1cfc2c87f603779f96b005a5a213dea04cb
Alex Johnson <johnsonav1992@gmail.com>
2024-01-17 06:21:31 -0600

    stylex works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import * as styleX from '@stylexjs/stylex'
+
+const styles = styleX.create({
+    test: {
+        width: '20px'
+        , height: '20px'
+        , backgroundColor: 'red'
+    }
+})
+
 function App () {
 
     return (
         <>
-            <div>
+            <div {...styleX.props(styles.test)}>
                 Hello
             </div>
         </>
